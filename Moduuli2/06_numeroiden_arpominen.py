89c57639db61407b852e67f73d7f92f788d03995
Tontsa <tontsa@nic.fi>
2024-08-22 10:51:54 +0300

    Updated

--- Moduuli2/06_numeroiden_arpominen.py
+++ Moduuli2/06_numeroiden_arpominen.py
@@ -9,15 +9,5 @@ print("Arvotaan kaksi erilaista numerolukon koodia:")
 
 from random import randint
 
-print("Noppa antaa:", randint(1, 6))
-
-for i in range(7):
-    print(randint(1, 40))
-
-rivi = []
-while len(rivi) < 7:
-    uusi = randint(1, 40)
-    if uusi not in rivi:
-        rivi.append(uusi)
-print(rivi)
-
+print(f"Kolmenumeroinen koodi (numerot välillä 0..9): {randint(0, 9)}{randint(0, 9)}{randint(0, 9)}")
+print(f"Nelinumeroinen koodi (numerot välillä 1..6): {randint(1, 6)}{randint(1, 6)}{randint(1, 6)}{randint(1, 6)}")
