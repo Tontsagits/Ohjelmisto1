efb0e72b9bab8eae5ce60158f15be2ea9ec3f82d
Tontsa <tontsa@nic.fi>
2024-08-22 11:35:13 +0300

    muokattu muuttujan nimeä

--- Moduuli2/05_keskiaikaiset_mitat.py
+++ Moduuli2/05_keskiaikaiset_mitat.py
@@ -6,10 +6,10 @@ Yksi naula on 32 luotia.
 Yksi luoti on 13,3 grammaa.
 '''
 
-leiviska = float(input("Anna leiviskät: "))
+leiviskat = float(input("Anna leiviskät: "))
 naulat = float(input("Anna naulat: "))
 luodit = float(input("Anna luodit: "))
 print("Massa nykymittojen mukaan:")
-massa = leiviska*20*32*13.3 + naulat*32*13.3 + luodit*13.3
+massa = leiviskat*20*32*13.3 + naulat*32*13.3 + luodit*13.3
 print(massa) # massan tarkastus
 print(f"{massa/1000:.0f} kilogrammaa ja {((massa/1000) - int(massa/1000)) * 1000:.2f} grammaa.")
