05b69423a8a039729ba7dcf817b75916045d52a2
Tontsa <tontsa@nic.fi>
2024-08-28 17:32:16 +0300

    updated muuttuja luku alkuarvo

---
+++ Moduuli4/02_tuumat_senteiksi_ilman_while_true.py
@@ -0,0 +1,7 @@
+# Ohjelmisto 1 - Moduuli 4 - Tehtävä 2 - Tuumat senteiksi, ilman while true booleania
+
+tuumat = 0
+while tuumat >= 0:
+    tuumat = float(input('Anna luku tuumina (negatiivinen luku lopettaa): '))
+    print(f'{tuumat} tuumaa on {tuumat/2.54:.2f} cm.')
+print('Heippa!')
