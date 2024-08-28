b9280eaa489d25562571785923bc0de8479b4c23
Tontsa <tontsa@nic.fi>
2024-08-28 14:03:51 +0300

    new

--- Moduuli4/05_kt_ja_ss_kysely.py
+++ Moduuli4/05_kt_ja_ss_kysely.py
@@ -1,3 +1,9 @@
 # Ohjelmisto 1 - Moduuli 4 - Tehtävä 5 - Käyttäjätunnus ja salasana kysely
 
+k_tunnus = ''
+s_sana = ''
+kerrat = 0
 
+while s_sana != 'rules' and k_tunnus != 'python' and kerrat < 6:
+    k_tunnus = input('Anna käyttäjätunnus: ')
+    s_sana = input('Anna sana? ')
