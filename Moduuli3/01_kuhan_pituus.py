ee8bd256acd1cff36ab1dfa47428b941c28c15de
Tontsa <tontsa@nic.fi>
2024-08-28 11:45:53 +0300

    new

--- Moduuli3/01_kuhan_pituus.py
+++ Moduuli3/01_kuhan_pituus.py
@@ -1,5 +1,11 @@
 # Moduuli3 - Tehtävä 1 - Kuhan pituus, ehtolause if
 
-print("Hello world!")
 
+kuhan_pituus = float(input('Anna Kuhan pituus (cm): '))
+
+if kuhan_pituus < 37:
+    alamittaisuus = 37 - kuhan_pituus
+    print(f"Kuha on {alamittaisuus:.1f} cm alamittainen, päästä se takaisin veteen.")
+else:
+    print("Kuha on riittävän iso. Voit pitää sen.")
 
