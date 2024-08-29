fb4b05e4698f336fe29e32561061bc94c52bf44e
Tontsa <tontsa@nic.fi>
2024-08-29 10:01:42 +0300

    updated while ilman True booleania

--- Moduuli4/03_pienin_suurin_luvut_ilman_while_true.py
+++ Moduuli4/03_pienin_suurin_luvut_ilman_while_true.py
@@ -1,28 +1,30 @@
 # Ohjelmisto 1 - Moduuli 4 - Tehtävä 3 - Pienin ja suurin luku annetuista, ilman while true
 
-# MITEN TÄMÄN TEKISI ILMAN WHILE TRUE BREAK YHDISTELMÄÄ
-
 pienin = ''
 suurin = ''
 alku = 0
-lopetus = 0
-luku = '0'
+luku = '  '
 
-while luku != '':
+while luku != ' ' and len(luku) != 0:
     luku = input('Anna luku (tyhjä lopettaa): ')
-    if alku == 0:
+    if alku == 0 and len(luku) != 0 and luku != ' ': # alustetaan muuttujat ensimmäisellä käyttäjän syöttämällä luvulla
         pienin = luku
         suurin = luku
-        alku += 1
-    if len(luku) == 0:
-        break
-    if luku == ' ':
-        break
-    elif luku < pienin:
+        alku += 1 # tässä käydään vain yhden kerran ohjelmassa
+    if len(luku) == 0: # jos käyttäjä syöttää ei mitään, lopetaan
+        continue
+    if luku == ' ': # jos käyttäjä syöttää välilyönnin, lopetetaan
+        continue
+    if luku < pienin: # jos uusi syötetty luku on pienempi
         pienin = luku
-    elif luku > suurin:
+    if luku > suurin: # jos uusi syötetty luku on suurempi
         suurin = luku
-print(f'Suurin luku {suurin} ja pienin luku on {pienin}.')
+# tulostetaan lopputulos vain jos käyttäjä on syöttänyt jotain käypiä arvoja
+# jos ei ole niin tulostetaan vain heippa
+if len(pienin) != 0 and len(suurin) != 0:
+    print(f'Suurin luku {suurin} ja pienin luku on {pienin}.')
+else:
+    print('Heippa!')
 
 
 
