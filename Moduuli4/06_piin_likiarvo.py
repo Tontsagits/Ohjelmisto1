d28e7aa99f8f5ef9673bcefd58d8cdd475bcf4b3
Tontsa <tontsa@nic.fi>
2024-08-29 10:43:36 +0300

    solved

--- Moduuli4/06_piin_likiarvo.py
+++ Moduuli4/06_piin_likiarvo.py
@@ -1,3 +1,35 @@
 # Ohjelmisto 1 - Moduuli 4 - Tehtävä 6 - Piin likiarvo
 
+'''
+Toteutetaan algoritmi piin (π) likiarvon laskemiseksi.
+Oletetaan, että A on yksikköympyrä eli ympyrä, jonka
+keskipiste on origossa ja jonka säde on yksi (1).
+Sen ympärille piirretään pienin mahdollinen neliö B
+siten, että ympyrä A jää kokonaan neliön sisäpuolelle.
+'''
 
+import random
+
+while True:
+    # arvottavien koordinaattien lukumäärä
+    N = int(input('Anna arvottavien pisteiden määrä (0 lopettaa): '))
+    if N == 0:
+        break
+    # arvontakerrat alussa
+    kerrat = 0
+    # ympyrän sisälle osuneet alussa
+    n = 0
+    # toteutetaan laskenta
+    while kerrat < N:
+        # arvotaan pisteiden x ja y koordinaatteja
+        x = random.uniform(0,1)
+        y = random.uniform(0,1)
+        # selvitetään osuuko piste ympyrän A sisälle
+        if x ** 2 + y ** 2 < 1:
+            # jos osuu kasvatetaan osumien määrää yhdellä
+            n += 1
+        # muistetaan kasvattaa tehtyjen arvontojen määrää aina yhdellä per kerta
+        kerrat += 1
+    # lasketaan pii:n likiarvo
+    pii = (4 * n) / N
+    print(f'Pii:n likiarvo on noin {pii:.5f}')
