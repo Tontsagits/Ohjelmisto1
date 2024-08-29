950595edea39fa9f83cde16e9eb68ce80bebaebb
Tontsa <tontsa@nic.fi>
2024-08-29 09:46:25 +0300

    updated with continue option

--- Moduuli4/02_tuumat_senteiksi_ilman_while_true.py
+++ Moduuli4/02_tuumat_senteiksi_ilman_while_true.py
@@ -3,5 +3,7 @@
 tuumat = 0
 while tuumat >= 0:
     tuumat = float(input('Anna luku tuumina (negatiivinen luku lopettaa): '))
+    if tuumat < 0: # jos käyttäjä syöttää negatiivisen lopetetaan heti, ei tulosteta negatiivista
+        continue # tämän voi jättää pois jos haluaa että yksi negatiivinen tulostuu
     print(f'{tuumat} tuumaa on {tuumat/2.54:.2f} cm.')
 print('Heippa!')
