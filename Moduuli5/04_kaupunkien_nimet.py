e8f7ed6f24585b495e495ebb90707fa9f811b5e6
Tontsa <tontsa@nic.fi>
2024-09-04 13:20:26 +0300

    Updated headings

--- Moduuli5/04_kaupunkien_nimet.py
+++ Moduuli5/04_kaupunkien_nimet.py
@@ -1,5 +1,10 @@
 # Ohjelmisto 1 - Moduuli 5 - Tehtävä 4 - Kaupunkien nimet
 
+kaupungit = []
 
+for _ in range(5):
+    kaupunki = input("Anna kaupungin nimi: ")
+    kaupungit.append(kaupunki)
 
-
+for stadi in kaupungit:
+    print(stadi)
