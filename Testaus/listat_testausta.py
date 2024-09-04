e2b16d00930d4f311fc822d3f987b171500672b8
Tontsa <tontsa@nic.fi>
2024-09-04 09:08:49 +0300

    New

---
+++ Testaus/listat_testausta.py
@@ -0,0 +1,12 @@
+# Lista olion testausta, listan elementit eli alkiot
+
+
+nimet = ["Viivi", "Ahmed", "Pekka", "Olga", "Mary", "Olavi", "Kermit", "Helmi", "Reipas"]
+
+print(nimet[3])
+print(nimet[1])
+print(nimet[-2])
+print(nimet[1:3])
+print(nimet[2:])
+print(nimet)
+
