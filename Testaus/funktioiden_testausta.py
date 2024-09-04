c1d2a3afa23f8dc16f28fec524feaa489aafcb88
Tontsa <tontsa@nic.fi>
2024-09-04 10:32:59 +0300

    Added local variables test

--- Testaus/funktioiden_testausta.py
+++ Testaus/funktioiden_testausta.py
@@ -1,5 +1,6 @@
 #  Funktioiden testausta
 
+# funktion parametrit
 def tervehdi(kerrat):
     for i in range(kerrat):
         print ("Hyvää päivää " + str(i+1) + ". kerran")
@@ -11,3 +12,13 @@ tervehdi(5)
 print ("Tervehditään vielä 2 kertaa lisää.")
 tervehdi(2)
 
+# funktion sisäiset, paikalliset muuttujat ja globaalit muuttujat
+def vaihda():
+    kaupunki = "Vantaa"
+    print("Funktiossa lopuksi: " + kaupunki)
+    return
+
+kaupunki = "Helsinki"
+print("Pääohjelmassa aluksi: " + kaupunki)
+vaihda()
+print("Pääohjelmassa lopuksi: " + kaupunki)
