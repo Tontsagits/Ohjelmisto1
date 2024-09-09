a006b30ee87919fbe0dc4472aab93db6098b7f65
Tontsa <tontsa@nic.fi>
2024-09-09 14:09:12 +0300

    Updated

--- Testaus/listat_testausta.py
+++ Testaus/listat_testausta.py
@@ -9,4 +9,28 @@ print(nimet[-2])
 print(nimet[1:3])
 print(nimet[2:])
 print(nimet)
+print()
 
+nimet2 = ["Makke", "Jokke", "Unto", "Mertsi", "Kaaleppi", "Viktoria", "Anselmi", "Mei", "Lin"]
+print(nimet2)
+print()
+
+nimet.append("Kuu-Ukko")
+print(nimet)
+print()
+
+nimet.sort()
+print(nimet)
+print()
+
+nimet.sort(reverse=True)
+print(nimet)
+print()
+
+nimet.extend(nimet2)
+print(nimet)
+print()
+
+for nimi in nimet:
+    print(nimi)
+print()
