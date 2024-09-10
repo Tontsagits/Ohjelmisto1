f36127cf0098d155131b71feb6aff8f54d71c42a
Tontsa <tontsa@nic.fi>
2024-09-10 09:10:45 +0300

    Updated for statement

--- Moduuli7/02_uudet_nimet.py
+++ Moduuli7/02_uudet_nimet.py
@@ -1,6 +1,15 @@
 # Ohjelmisto 1 - Moduuli 7 - Tehtävä 2 - Uudet nimet
 
-nimi = input("Anna nimi")
+nimet = set()
 
-
-
+while True:
+    nimi = input("Anna nimi (tyhjä lopetta): ")
+    if nimi == "":
+        break
+    if nimi in nimet:
+        print("Aiemmin syötetty nimi")
+    else:
+        print("Uusi nimi")
+        nimet.add(nimi)
+for alkio in nimet:
+    print(alkio)
