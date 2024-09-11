d3c0688732eab1761e2660913bb183ec139f66c1
Tontsa <tontsa@nic.fi>
2024-09-11 18:38:46 +0300

    Updated with order by

--- Testaus/tietokanta_testausta_lentokentta_2.py
+++ Testaus/tietokanta_testausta_lentokentta_2.py
@@ -7,12 +7,13 @@ import mysql.connector
 # omat funktiot
 
 def hae_lentoketta_maa_koodilla(maa):
-    sql = f"SELECT id, ident, name, municipality FROM airport WHERE iso_country = %s;"
-    print(sql)
+    sql = f"SELECT id, ident, name, municipality FROM airport WHERE iso_country = %s order by ident;"
+    # print(sql)
     kursori = db_lentopeli.cursor()
     kursori.execute(sql, (maa,))
     tulos = kursori.fetchall()
     if kursori.rowcount > 0 :
+        # print(tulos)
         return tulos
     else:
         return None
@@ -32,6 +33,6 @@ code = input('Syötä lentokentän maatunnus: ')
 lentokentta = hae_lentoketta_maa_koodilla(code)
 if lentokentta is not None:
     for rivi in lentokentta:
-        print(f"{rivi[1]} on {rivi[2]} ja sijaitsee paikkakunnalla {rivi[4]}.")
+        print(f"{rivi[1]} on {rivi[2]} ja sijaitsee paikkakunnalla {rivi[3]}.")
 else:
     print('Lentokenttiä ei löytynyt')
