e5d5da95154518934fd782a38a0655b7b9a6068b
Tontsa <tontsa@nic.fi>
2024-09-11 19:42:14 +0300

    New

---
+++ Moduuli8/01_hae_lentokentta.py
@@ -0,0 +1,39 @@
+# Ohjelmisto 1 - Moduuli 8 - Tehtävä 3 - Hae lentokenttä
+
+# ladattavat kirjastot
+
+import mysql.connector
+
+# omat funktiot
+
+def hae_lentokentta_kentta_koodilla(tunnus):
+    kysely = f"SELECT id, ident, name, municipality FROM airport WHERE ident = %s;"
+    # print(kysely)
+    kursori = db_lentopeli.cursor()
+    kursori.execute(kysely, (tunnus,))
+    tulos = kursori.fetchall()
+    if kursori.rowcount > 0 :
+        # print(tulos)
+        return tulos
+    else:
+        return None
+
+
+# main pääohjelma
+
+db_lentopeli = mysql.connector.connect(
+    host='localhost',
+    port= 3306,
+    database='flight_game',
+    user='tontsah',
+    password='tontsah1234',
+    autocommit=True
+)
+
+icaotunnus = input('Syötä lentokentän ICAO tunnus: ')
+lentokentta = hae_lentoketta_maa_koodilla(icaotunnus)
+if lentokentta is not None:
+    for rivi in lentokentta:
+        print(f"{rivi[1]} on {rivi[2]} ja sijaitsee paikkakunnalla {rivi[3]}.")
+else:
+    print('Lentokenttiä ei löytynyt')
