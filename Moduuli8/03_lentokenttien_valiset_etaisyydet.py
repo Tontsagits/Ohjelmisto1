460a1d12088b79dc1d4e060bd4caed1764bfaeef
Tontsa <tontsa@nic.fi>
2024-09-11 20:38:07 +0300

    Solved

--- Moduuli8/03_lentokenttien_valiset_etaisyydet.py
+++ Moduuli8/03_lentokenttien_valiset_etaisyydet.py
@@ -4,19 +4,28 @@
 # ladattavat kirjastot
 
 import mysql.connector
-import geopy
+import geopy.distance
 
 # omat funktiot
 
-def hae_lentokentta_koodilla(tunnus):
-    kysely = f"SELECT id, ident, name, municipality FROM airport WHERE ident = %s;"
+def laske_kenttien_etaisyys(tunnus1, tunnus2):
+    kysely = f"SELECT ident, latitude_deg, longitude_deg FROM airport WHERE ident = %s;"
     # print(kysely)
     kursori = db_lentopeli.cursor()
-    kursori.execute(kysely, (tunnus,))
-    tulos = kursori.fetchall()
+    kursori.execute(kysely, (tunnus1,))
+    tulos1 = kursori.fetchone()
+    # print(tulos1)
+    koordinaatti1 = tulos1[1], tulos1[2]
+    # print(koordinaatti1)
+    kursori.execute(kysely, (tunnus2,))
+    tulos2 = kursori.fetchone()
+    # print(tulos2)
+    koordinaatti2 = tulos2[1], tulos2[2]
+    # print(koordinaatti2)
+    distance = geopy.distance.geodesic(koordinaatti1, koordinaatti2).kilometers
     if kursori.rowcount > 0 :
-        # print(tulos)
-        return tulos
+        # print(f"{tulos1[0]}, {tulos2[0]}, {distance}")
+        return tulos1[0], tulos2[0], distance
     else:
         return None
 
@@ -36,6 +45,6 @@ icaot1 = input('Syötä ensimmäisen lentokentän ICAO tunnus: ')
 icaot2 = input('Syötä ensimmäisen lentokentän ICAO tunnus: ')
 etaisyys = laske_kenttien_etaisyys(icaot1, icaot2)
 if etaisyys is not None:
-    print(f"{rivi[1]} on {rivi[2]} ja sijaitsee paikkakunnalla {rivi[3]}.")
+    print(f"Kenttien {etaisyys[0]} ja {etaisyys[1]} välinen etäisyys on {etaisyys[2]:.2f} km.")
 else:
     print('Lentokenttiä ei löytynyt.')
